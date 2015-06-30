edd3bcad7b426e833a3c95e69b660f53e7ca2dd5
Akshay Khole <akshay@reversehack.in>
2015-06-30 14:17:56 -0700

    Initial Commit

---
+++ iTodo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iTodo
+//
+//  Created by Akshay Khole on 6/30/15.
+//  Copyright (c) 2015 Awesome Apps. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
